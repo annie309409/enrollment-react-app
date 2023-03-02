0bc92b83cfa1112128c8bcc0dc2a639b1b7713e0
annie309409 <pjatopaz1@naver.com>
2023-03-02 15:54:01 +0900

    enrollment app 초기화

---
+++ README.md
@@ -0,0 +1,3 @@
+# Enrollment-react-app
++ react 16.14.0
++ react-dom 16.14.0
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>enrollment app</h1>
+  </React.StrictMode>,document.getElementById('app')
+);
+
