e40d008f3409ed3575b4e36dfd1479549b2d2f48
annie309409 <pjatopaz1@naver.com>
2023-03-02 16:13:19 +0900

    React App 실행테스트

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>enrollment app</h1>
-  </React.StrictMode>,document.getElementById('app')
+    <h1>Just React</h1>
+  </React.StrictMode>,document.querySelector('#app')
+);
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,document.querySelector('#root')
 );
 
