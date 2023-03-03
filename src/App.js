2951267b5825c12e16f6355e793de983dabfe2bd
annie309409 <pjatopaz1@naver.com>
2023-03-03 10:59:44 +0900

    참가 가능 인원수표시 1

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ import EnrollmentForm from "./components/enrollmentForm";
 const App = ()=>{
     const [program,setProgram]=useState('UG');
     const [prgname,setPrgname] = useState('UnderGraduate(대학교)');
+    const [seats,setSeats] = useState(100);
     const handleChange = (e) => {
         setProgram(e.target.value);
         setPrgname(e.target.options[e.target.selectedIndex].text);
     };
 
 
+    //바꿔줄 수 있는 함수를 넘겨야 함
+    // 참가 가능 이원수를 변경하는 함수
+    const setUpdateSeats=(modifySeat)=>{
+        setSeats(modifySeat);
+    };
+
     return(
         <div className="app">
             <div className="programs">
+                <label>프로그램 참가가능 인원 수 : {seats}</label>
+                <br/>
                 <label htmlFor="sel">프로그램 종류: </label>
                 {/* program의 이름으로 ug, pg가 들어감*/}
                 <select name="sel" id="sel" className="appDropDowns" onChange={handleChange} value={program}>
@@ -21,7 +30,7 @@ const App = ()=>{
                     <option value="PG">PostGraduate(대학원)</option>
                 </select>
             </div>
-            <EnrollmentForm chosenProgram={prgname}/>
+            <EnrollmentForm chosenProgram={prgname} cSeat={seats} setUpdateSeats={setUpdateSeats} />
         </div>
     );
 }
